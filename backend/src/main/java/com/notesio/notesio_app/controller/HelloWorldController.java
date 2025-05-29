3ec3e2fdc676deecb0454774bebe2e4bea069b46
ammes <pedro.ammes@gmail.com>
2025-05-29 09:59:32 -0300

    Added .env in both front and back end

--- backend/src/main/java/com/notesio/notesio_app/controller/HelloWorldController.java
+++ backend/src/main/java/com/notesio/notesio_app/controller/HelloWorldController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.notesio.notesio_app.domain.User;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
@@ -11,4 +17,10 @@ public class HelloWorldController {
     public String Map() {
         return "Hello World";
     }
+
+    @PostMapping("/yourname")
+    public String postMethodName(@RequestBody User body) {
+        return "Your name is: " + body.getName();  
+    }
+    
 }
---
+++ backend/src/main/java/com/notesio/notesio_app/domain/User.java
@@ -0,0 +1,14 @@
+package com.notesio.notesio_app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    public String name;
+    public String email;
+
+}
